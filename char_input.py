07d9af4080a008153ec77426a340abfbf1572a63
Sarah Stephens <quidamspot@gmail.com>
2024-04-18 14:26:18 -0700

    Completed extra portion

--- char_input.py
+++ char_input.py
@@ -4,6 +4,12 @@ Create a program that asks the user to enter their name and their age.
 Print out a message addressed to them that tells them the year that they will turn 100 years old. 
 Note: for this exercise, the expectation is that you explicitly write out the year 
 (and therefore be out of date the next year).
+Extras:
+Add on to the previous program by asking the user for another number 
+and printing out that many copies of the previous message. 
+(Hint: order of operations exists in Python)
+Print out that many copies of the previous message on separate lines. 
+(Hint: the string "\n is the same as pressing the ENTER button)
 '''
 
 # importing datetime module for today()  
@@ -22,6 +28,8 @@ def calcFuture100(age):
 
 if __name__ == "__main__":
     name = input("what is your name? \n")
-    age = input("What is your current age? \n")
-    year = calcFuture100(int(age))
-    print(f"Hi {name}, being {age} now, your will be 100 in {year}")
+    age = int(input("What is your current age? \n"))
+    num = int(input("How many times should I print the message? \n"))
+    year = calcFuture100(age)
+    for i in range(num):
+        print(f"Hi {name}, being {age} now, your will be 100 in {year}")
